10d2140e8f5fd3b6582098fdde56d0163d9cb3cd
Bradley Munday <99284020+bmunday3@users.noreply.github.com>
2022-12-05 10:23:51 -0500

    Update config.py

--- flask-app/config.py
+++ flask-app/config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'modzy modelops are incredible'
 
-	API_URL = os.environ.get('API_URL') or "https://demo.modzy.engineering"
+	API_URL = os.environ.get('API_URL') or "https://<your-modzy-instance-domain>"
 	API_KEY = os.environ.get('API_KEY') or "API_KEY_Public.API_KEY_Private"
 	
 	DEBUG =  os.environ.get('FLASK_DEBUG') or 0
